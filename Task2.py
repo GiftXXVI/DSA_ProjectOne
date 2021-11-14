7dc6657272026d8acd973447515d4c7d04d71087
GiftXXVI <gift.chimphonda@outlook.com>
2021-11-14 12:24:34 +0200

    Refactored Task2

--- Task2.py
+++ Task2.py
@@ -28,20 +28,20 @@ for call in calls:
 
 numbers = call_diallers + call_recipients
 unique_numbers = []
-for number in numbers:
-    if number not in unique_numbers:
-        unique_numbers.append(number)
 summary = {}
 maxindex = ''
 maxvalue = 0
-for number in unique_numbers:
-    summary[number] = 0
-    for call in calls:
-        if call[0] == number or call[1] == number:
-            summary[number] += int(call[3])
-    if summary[number] > maxvalue:
-        maxvalue = summary[number]
-        maxindex = number
+for number in numbers:
+    if number not in unique_numbers:
+        unique_numbers.append(number)
+        summary[number] = 0
+        for call in calls:
+            if call[0] == number or call[1] == number:
+                summary[number] += int(call[3])
+        if summary[number] > maxvalue:
+            maxvalue = summary[number]
+            maxindex = number
+
 
 print(f'''
 {maxindex} spent the longest time, {maxvalue} seconds, on the phone during September 2016. 
